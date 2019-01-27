2b8dc3cb7446b2340e9f58765bf9712af38fa464
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-01-27 13:38:50 -0500

    index.js, index.html, main.js para creacion de cuenta y logueo para red social

--- src/main.js
+++ src/main.js
@@ -1,5 +1,34 @@
-// Este es el punto de entrada de tu aplicacion
+// import myFunction from './lib/index.js';
+// myFunction();
+// Obtener los elementos
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const btnLogIn = document.getElementById('btnLogIn');
+const btnSignUp = document.getElementById('btnSignUp');
+const btnLogOut = document.getElementById('btnLogOut');
+const auth = firebase.auth();
 
-import { myFunction } from './lib/index.js';
+// Hacer evento login
+btnLogIn.addEventListener('click', e => {
+  auth;
+  const promise = auth.signInWithEmailAndPassword(txtEmail.value,txtPassword.value);
+promise.catch(e => console.log(e.message));
+});
 
-myFunction();
+// Creando cuenta
+// TO DO: Verificarq ue sea un correo real
+btnSignUp.addEventListener('click', e => {
+ auth;
+const promise = auth.createUserWithEmailAndPassword(txtEmail.value,txtPassword.value);
+promise.catch(e => console.log(e.message));
+});
+
+// Agregando un listener en tiempo real
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser){
+    console.log(firebaseUser);
+  } else {
+    console.log('not logged in');
+  }
+});
+
