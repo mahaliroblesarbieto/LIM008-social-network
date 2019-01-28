44ce090a8fdc2be1e02abd330b6af1b5ed407db7
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-01-28 07:22:59 -0500

    authenticateWithEmailAndPassword & CreateUserWithEmailAndPassword, index.js(funciones) y main.js(DOM de funciones)

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,68 @@
 // // aqui exportaras las funciones que necesites
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const email = document.getElementById('txtEmail');
+const password = document.getElementById('txtPassword');
+
+export const createUserWithEmailAndPassword = () => {
+  firebase.auth().createUserWithEmailAndPassword(email.value,password.value)
+    .catch(function(error) {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      })
+    };
+
+export const authenticateWithEmailAndPassword = () => {
+    firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+    .catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  if (errorCode === 'auth/wrong-password') {
+    alert('Wrong password.');
+  } else {
+    alert(errorMessage);
+  }
+  console.log(error);
+})
+};
+
--- src/main.js
+++ src/main.js
@@ -1,34 +1,16 @@
-// import myFunction from './lib/index.js';
-// myFunction();
+
+
+
+
+
+import { authenticateWithEmailAndPassword, createUserWithEmailAndPassword }  from './lib/index.js';
 // Obtener los elementos
-const txtEmail = document.getElementById('txtEmail');
-const txtPassword = document.getElementById('txtPassword');
 const btnLogIn = document.getElementById('btnLogIn');
 const btnSignUp = document.getElementById('btnSignUp');
 const btnLogOut = document.getElementById('btnLogOut');
-const auth = firebase.auth();
 
-// Hacer evento login
-btnLogIn.addEventListener('click', e => {
-  auth;
-  const promise = auth.signInWithEmailAndPassword(txtEmail.value,txtPassword.value);
-promise.catch(e => console.log(e.message));
-});
+btnSignUp.addEventListener('click', createUserWithEmailAndPassword);
+btnLogIn.addEventListener('click', authenticateWithEmailAndPassword);
 
-// Creando cuenta
-// TO DO: Verificarq ue sea un correo real
-btnSignUp.addEventListener('click', e => {
- auth;
-const promise = auth.createUserWithEmailAndPassword(txtEmail.value,txtPassword.value);
-promise.catch(e => console.log(e.message));
-});
 
-// Agregando un listener en tiempo real
-firebase.auth().onAuthStateChanged(firebaseUser => {
-  if(firebaseUser){
-    console.log(firebaseUser);
-  } else {
-    console.log('not logged in');
-  }
-});
 
