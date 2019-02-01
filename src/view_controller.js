dbacb150a1339d1dbac97538b0431d8a9d01733c
SandraMartinezCorcino <sandra.martinez.corcino@gmail.com>
2019-02-01 16:45:38 -0500

    En el archivo view_controller.js se agrego location.hash

--- src/view_controller.js
+++ src/view_controller.js
@@ -1,31 +1,31 @@
 import { Popup, createDocumentUID } from "./lib/index.js";
+import { changeTmp } from "./lib/app.js";
 const changeHash = (hash) =>  {
   location.hash = hash;
   console.log(location.hash);
 }
 export const loginFacebook = () => {
-Popup()
-.then(result => {
-  console.log(result.user.uid);
-    const uid = result.user.uid;
-    const user = result.user.displayName;  
-    const email = result.user.email;
-    changeHash('/home');
-    console.log(location.hash);
-    /*createDocumentUID(uid, {uid,user,email})
-     .then(() => { 
-      console.log(location.hash);
-      })
-    .catch(() => {})*/
-   }).catch(error => {
-    const errorCode = error.code;
-    if (errorCode === 'auth/account-exists-with-different-credential') {
-      return 'Es el mismo usuario';
-    }
-  })
-
+  Popup()
+    .then(result => {
+      console.log(result.user.uid);
+      const uid = result.user.uid;
+      const user = result.user.displayName;  
+      const email = result.user.email;
+      location.hash = '#/home';
+      changeTmp(location.hash);
+    }).catch(error => {
+      const errorCode = error.code;
+      if (errorCode === 'auth/account-exists-with-different-credential') {
+        return 'Es el mismo usuario';
+      }
+    });
 };
 
+  /*createDocumentUID(uid, {uid,user,email})
+      .then(() => { 
+        console.log(location.hash);
+        })
+      .catch(() => {})*/
 
 
 
