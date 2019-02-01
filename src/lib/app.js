d7046e26d11677c526c37be720dcf5f531925bda
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-02-01 09:49:51 -0500

    actualizando codigo

---
+++ src/lib/app.js
@@ -0,0 +1,18 @@
+export {changeTmp};
+import {objTemp} from './tempString.js';
+
+const changeTmp = (hash) => {
+  if (hash === '') {
+    viewTmp('#/home');
+  } else {
+    viewTmp(hash); 
+  }
+};
+    
+const viewTmp = (routers) => {
+  let router = routers.substr(2, routers.length - 2);
+  console.log(router);
+  const container = document.getElementById('container');
+  container.innerHTML = '';
+  container.appendChild(objTemp[router]());
+};
