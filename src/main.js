a9794f3b55f9d6a7270aa8b776fa1247c93ba4f3
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-02-01 17:15:55 -0500

    cmabios para createuserwithemailandpassword

--- src/main.js
+++ src/main.js
@@ -5,5 +5,11 @@ import { authenticateGoogleAccount,
 
 document.getElementById('button-google').addEventListener('click', authenticateGoogleAccount);
 document.getElementById('button-facebook').addEventListener('click', authenticateFacebookAccount);
-document.getElementById('btnSignUp').addEventListener('click', createUserWithEmailAndPassword);
+// document.getElementById('btnSignUp').addEventListener('click', createUserWithEmailAndPassword);
 document.getElementById('btnLogIn').addEventListener('click', authenticateWithEmailAndPassword);
+
+export const signUpOnSubmit = () => {
+  const email = document.querySelector('#emailSignUp').value;
+  const password = document.querySelector('#passwordSignUp').value;
+  createUserWithEmailAndPassword(email,password);
+}
