b749904bd0ee9dcd17088341ca944185dce5c2e2
SandraMartinezCorcino <sandra.martinez.corcino@gmail.com>
2019-02-04 17:31:12 -0500

    Se ordeno los archivos js del index.html

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import {initRouter} from './lib/index.js';
 import { initFirebase } from './lib/ConfigFireBase.js'
-
-// document.getElementById('button-google').addEventListener('click', authenticateWithGoogle);
-// document.getElementById('btnSignUp').addEventListener('click', goToRegister);
-// document.getElementById('btnLogIn').addEventListener('click', authenticateWithEmailAndPassword);
-// document.getElementById('button-facebook').addEventListener('click', authenticateFacebook);
-
 const init = () => {
   initFirebase();
   initRouter();
 }
-
 window.addEventListener('load', init)
 
