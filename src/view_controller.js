9eca5f071a92de0ba491838632f8aac24ef68d20
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-02-06 10:53:20 -0500

    se adiciono a fncion publicar, la opcion para que el usuario no pueda publicar en caso que el text area este vacio/index.js

--- src/view_controller.js
+++ src/view_controller.js
@@ -131,9 +131,11 @@ export const publish = () => {
   const user = firebase.auth().currentUser.displayName;
   const enteredText = document.querySelector('#entered-text').value;
   const postType = document.querySelector('#post-type').value;
-  savePublication(user, enteredText, postType)
-    .then((data) => consultPost(data))
-    .catch(error => {
-      console.error(`Error creando el post => ${error}`);
-    });
+  if (enteredText !== '') {
+    savePublication(user, enteredText, postType)
+      .then((data) => consultPost(data))
+      .catch(error => {
+        console.error(`Error creando el post => ${error}`);
+      });
+  }
 };
