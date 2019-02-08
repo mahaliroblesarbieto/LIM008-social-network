8b09f37b8ac39cda504970493ee39157b1da8127
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-02-08 14:36:46 -0500

    cambios para ordenado de index y hacer test

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-import {changeHash} from '../view_controller.js';
+import { itemNote} from '../view_controller.js';
 export const authenticateGoogleAccount = () => 
   firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
   // .addScope('https://www.googleapis.com/auth/plus.login'));
@@ -36,7 +36,6 @@ export const deletePost = (postId) => {
     .then(() => {
     // consultPost();
     //      abrir una ventana modal que pida confirmar
-    //      imprimir la nueva data en los templates
       console.log('Es exitoso');
     })
     .catch((error) => {
@@ -44,123 +43,10 @@ export const deletePost = (postId) => {
     });
 };
 
-const newAddLike = (id, newLike) => {
+export const newAddLike = (id, newLike) => {
   firebase.firestore().collection('Posts').doc(id).update({
     'likes': newLike
   });
-};
-
-const itemNote = (objNote) => {
-  const liElement = document.createElement('li');
-  const date = (objNote.date.toDate()).toString();
-  const newDate = date.substr(4, date.length - 37);
-
-  liElement.innerHTML = `
-  <div class="row">
- <div class="col-12 col-s-12">
-   <p>${objNote.uid}, ${newDate} <img id ="typeimage"> </img></p>
- </div>
-</div>
-<div class="row">
- <div class="col-12 col-s-12">
-   <span>${objNote.text}</span>
- </div>
-</div>
-<div class="row">
- <div class="col-12 col-s-12">
-   <div class="col-2 col-s-2">
-     <button type = "button" id = "btnUpdate-${objNote.id}"  class="type logIn border">Editar</button>
-   </div>
-   <div class="col-2 col-s-2">
-     <button type = "button" id = "btnDelete-${objNote.id}"  class="type logIn border">Eliminar</button>
-   </div>
-   <div class="col-2 col-s-2">
-     <button type = "button" id = "btnLike-${objNote.id}"  class="type logIn border"><p id="number"> ${objNote.likes}</p>Me gusta</button>
-   </div>
-   <div class="col-2 col-s-2">
-   </div>
-   <div class="col-2">
-   </div>
-   <div class="col-2">
-   </div>
- </div>
-</div>
-<div id="myModal" class="modal">
- <!-- Modal content -->
- <div class="modal-content">
-   <textarea rows="4" cols="50" id="post-content">
-     ${objNote.text}
-   </textarea>
-   <div class="row">
-     <div class="col-12 col-s-12">
-       <div class="col-2 col-s-2">
-         <button type = "button" id = "btn-update-content"  class="type logIn border">Editar</button>
-       </div>
-       <div class="col-2 col-s-2">
-         <button type = "button" id = "btn-close-modal"  class="type logIn border">Cerrar</button>
-       </div>
-     </div>
-   </div>
- </div>
-</div>
-<div id="myModaldos" class="modal">
- <!-- Modal content -->
- <div class="modal-content">
-    <p>¿Estas seguro que deseas eliminar? </p>
-   <div class="row">
-     <div class="col-12 col-s-12">
-       <div class="col-2 col-s-2">
-         <button type = "button" id = "btn-delete-confirm"  class="type logIn border">Si</button>
-       </div>
-       <div class="col-2 col-s-2">
-         <button type = "button" id = "btn-delete-negative"  class="type logIn border">No</button>
-       </div>
-     </div>
-   </div>
- </div>
-</div>`;
-  if (objNote.public === 'true') {
-    liElement.querySelector('#typeimage').src = 'img/world.png';
-  } else {
-    liElement.querySelector('#typeimage').src = 'img/animal-prints.png';
-  }
-  
-  const btnUpdatePost = liElement.querySelector(`#btnUpdate-${objNote.id}`);
-  const modalUpdatePost = liElement.querySelector('#myModal');
-  btnUpdatePost.addEventListener('click', () => {
-    modalUpdatePost.style.display = 'block';
-  });
-  const btnUpdateContent = liElement.querySelector('#btn-update-content');
-  btnUpdateContent.addEventListener('click', () => {
-    const textNew = liElement.querySelector('#post-content').value;
-    UpdatedPost(objNote.id, textNew);
-  });
-
-  const btnCancelUpdate = liElement.querySelector('#btn-close-modal');
-  btnCancelUpdate.addEventListener('click', () => {
-    modalUpdatePost.style.display = 'none';
-  });
-  const btndeletePost = liElement.querySelector(`#btnDelete-${objNote.id}`);
-  const modalConfirmDelete = liElement.querySelector('#myModaldos');
-  btndeletePost.addEventListener('click', () => {
-    modalConfirmDelete.style.display = 'block';
-  });
-  const btnConfirmDelete = liElement.querySelector('#btn-delete-confirm');
-  btnConfirmDelete.addEventListener('click', () => {
-    deletePost(objNote.id);
-  });
-
-  const btnDeniedDelete = liElement.querySelector('#btn-delete-negative');
-  btnDeniedDelete.addEventListener('click', () => { 
-    changeHash('#/home');
-  });
-  let number = objNote.likes;
-  const btnLike = liElement.querySelector(`#btnLike-${objNote.id}`);
-  btnLike.addEventListener('click', () => {
-    number = number + 1 ;
-    newAddLike(objNote.id, number);
-  });
-  return liElement;
 };
 
 export const consultPost = () => {
@@ -181,13 +67,11 @@ export const consultPost = () => {
 };
 
 export const consultTypePost = (type) => {
-  console.log('a');
   firebase.firestore()
     .collection('Posts')
     .orderBy('date', 'desc')
     .where('public', '==', type)
     .onSnapshot(querySnapshot => {
-      console.log('b');
       const ul = document.querySelector('#notes-list');
       ul.innerHTML = '';
       const data = [];
--- src/view_controller.js
+++ src/view_controller.js
@@ -4,7 +4,10 @@ import {authenticateGoogleAccount,
   authenticateFacebookAccount,
   closeSesion,
   savePublication,
-  consultPost} from './lib/index.js';
+  consultPost,
+  UpdatedPost,
+  deletePost,
+  newAddLike } from './lib/index.js';
 import {changeTmp} from './lib/app.js';
 
 export const changeHash = (hash) => {
@@ -157,4 +160,116 @@ export const publish = () => {
       .then((data) => consultPost(data))
       .catch({});
   }
+};
+
+export const itemNote = (objNote) => {
+  const liElement = document.createElement('li');
+  const date = (objNote.date.toDate()).toString();
+  const newDate = date.substr(4, date.length - 37);
+
+  liElement.innerHTML = `
+  <div class="row">
+ <div class="col-12 col-s-12">
+   <p>${objNote.uid}, ${newDate} <img id ="typeimage"> </img></p>
+ </div>
+</div>
+<div class="row">
+ <div class="col-12 col-s-12">
+   <span>${objNote.text}</span>
+ </div>
+</div>
+<div class="row">
+ <div class="col-12 col-s-12">
+   <div class="col-2 col-s-2">
+     <button type = "button" id = "btnUpdate-${objNote.id}"  class="type logIn border">Editar</button>
+   </div>
+   <div class="col-2 col-s-2">
+     <button type = "button" id = "btnDelete-${objNote.id}"  class="type logIn border">Eliminar</button>
+   </div>
+   <div class="col-2 col-s-2">
+     <button type = "button" id = "btnLike-${objNote.id}"  class="type logIn border"><p id="number"> ${objNote.likes}</p>Me gusta</button>
+   </div>
+   <div class="col-2 col-s-2">
+   </div>
+   <div class="col-2">
+   </div>
+   <div class="col-2">
+   </div>
+ </div>
+</div>
+<div id="myModal" class="modal">
+ <!-- Modal content -->
+ <div class="modal-content">
+   <textarea rows="4" cols="50" id="post-content">
+     ${objNote.text}
+   </textarea>
+   <div class="row">
+     <div class="col-12 col-s-12">
+       <div class="col-2 col-s-2">
+         <button type = "button" id = "btn-update-content"  class="type logIn border">Editar</button>
+       </div>
+       <div class="col-2 col-s-2">
+         <button type = "button" id = "btn-close-modal"  class="type logIn border">Cerrar</button>
+       </div>
+     </div>
+   </div>
+ </div>
+</div>
+<div id="myModaldos" class="modal">
+ <!-- Modal content -->
+ <div class="modal-content">
+    <p>¿Estas seguro que deseas eliminar? </p>
+   <div class="row">
+     <div class="col-12 col-s-12">
+       <div class="col-2 col-s-2">
+         <button type = "button" id = "btn-delete-confirm"  class="type logIn border">Si</button>
+       </div>
+       <div class="col-2 col-s-2">
+         <button type = "button" id = "btn-delete-negative"  class="type logIn border">No</button>
+       </div>
+     </div>
+   </div>
+ </div>
+</div>`;
+  if (objNote.public === 'true') {
+    liElement.querySelector('#typeimage').src = 'img/world.png';
+  } else {
+    liElement.querySelector('#typeimage').src = 'img/animal-prints.png';
+  }
+  
+  const btnUpdatePost = liElement.querySelector(`#btnUpdate-${objNote.id}`);
+  const modalUpdatePost = liElement.querySelector('#myModal');
+  btnUpdatePost.addEventListener('click', () => {
+    modalUpdatePost.style.display = 'block';
+  });
+  const btnUpdateContent = liElement.querySelector('#btn-update-content');
+  btnUpdateContent.addEventListener('click', () => {
+    const textNew = liElement.querySelector('#post-content').value;
+    UpdatedPost(objNote.id, textNew);
+  });
+
+  const btnCancelUpdate = liElement.querySelector('#btn-close-modal');
+  btnCancelUpdate.addEventListener('click', () => {
+    modalUpdatePost.style.display = 'none';
+  });
+  const btndeletePost = liElement.querySelector(`#btnDelete-${objNote.id}`);
+  const modalConfirmDelete = liElement.querySelector('#myModaldos');
+  btndeletePost.addEventListener('click', () => {
+    modalConfirmDelete.style.display = 'block';
+  });
+  const btnConfirmDelete = liElement.querySelector('#btn-delete-confirm');
+  btnConfirmDelete.addEventListener('click', () => {
+    deletePost(objNote.id);
+  });
+  const btnDeniedDelete = liElement.querySelector('#btn-delete-negative');
+  btnDeniedDelete.addEventListener('click', () => { 
+    modalConfirmDelete.style.display = 'none';
+  });
+  let number = objNote.likes;
+  const btnLike = liElement.querySelector(`#btnLike-${objNote.id}`);
+  btnLike.addEventListener('click', () => {
+    number = number + 1 ;
+    newAddLike(objNote.id, number);
+  });
+  return liElement;
 };
