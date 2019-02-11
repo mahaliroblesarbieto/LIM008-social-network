0d5af348ac54f058646c6a09d9783881cfd980f9
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-02-11 15:44:59 -0500

    colores definitivos

--- src/lib/tempString.js
+++ src/lib/tempString.js
@@ -50,7 +50,7 @@ const objTemp = {
   },
   home: () => {
     const tmpl = 
-    `<header class="login">
+    `<header class="color">
     <div class="row" id="title">
       <div class="col-12 col-s-12 center title">
       <img class = "left" src = "img/menu.png"></img>
